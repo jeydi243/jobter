1e734d804c161a3a88ff9dc755c09aa466ad1586
jeydi243 <kadiongoe@yahoo.fr>
2020-02-25 00:32:04 +0100

    Seeker controller changed

--- src/seeker/seeker.controller.ts
+++ src/seeker/seeker.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Res, HttpStatus, Post, Body, Put, Query, NotFoundException, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Post, Body, NotFoundException, Param } from '@nestjs/common';
 import { SeekerService } from './seeker.service';
 import { CreateSeekerDTO } from './dto/create-seeker.dto';
 
 @Controller('customer')
-export class CustomerController {
-    constructor(private customerService: SeekerService) { }
+export class SeekerController {
+    constructor(private seekerService: SeekerService) { }
 
     // add a customer
     @Post('/create')
-    async addCustomer(@Res() res, @Body() createCustomerDTO: CreateSeekerDTO) {
-        const customer = await this.customerService.addCustomer(createCustomerDTO);
+    async addSeeker(@Res() res, @Body() createCustomerDTO: CreateSeekerDTO) {
+        const customer = await this.seekerService.addCustomer(createCustomerDTO);
         return res.status(HttpStatus.OK).json({
             message: "Customer has been created successfully",
             customer
@@ -19,14 +19,14 @@ export class CustomerController {
     // Retrieve customers list
     @Get('customers')
     async getAllCustomer(@Res() res) {
-        const customers = await this.customerService.getAllCustomer();
+        const customers = await this.seekerService.getAllCustomer();
         return res.status(HttpStatus.OK).json(customers);
     }
 
     // Fetch a particular customer using ID
     @Get('customer/:customerID')
     async getCustomer(@Res() res, @Param('customerID') customerID) {
-        const customer = await this.customerService.getCustomer(customerID);
+        const customer = await this.seekerService.getCustomer(customerID);
         if (!customer) throw new NotFoundException('Customer does not exist!');
         return res.status(HttpStatus.OK).json(customer);
     }
