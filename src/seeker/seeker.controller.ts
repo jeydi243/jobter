f0b83dd252b8222bb96890f47a83bd7c0712202a
jeydi243 <kadiongoe@yahoo.fr>
2020-02-25 00:34:45 +0100

    Seeker controller renamed var

--- src/seeker/seeker.controller.ts
+++ src/seeker/seeker.controller.ts
@@ -9,25 +9,25 @@ export class SeekerController {
     // add a customer
     @Post('/create')
     async addSeeker(@Res() res, @Body() createCustomerDTO: CreateSeekerDTO) {
-        const customer = await this.seekerService.addCustomer(createCustomerDTO);
+        const seeker = await this.seekerService.addCustomer(createCustomerDTO);
         return res.status(HttpStatus.OK).json({
             message: "Customer has been created successfully",
-            customer
+            seeker
         })
     }
 
     // Retrieve customers list
     @Get('customers')
     async getAllCustomer(@Res() res) {
-        const customers = await this.seekerService.getAllCustomer();
-        return res.status(HttpStatus.OK).json(customers);
+        const seekers = await this.seekerService.getAllCustomer();
+        return res.status(HttpStatus.OK).json(seekers);
     }
 
     // Fetch a particular customer using ID
     @Get('customer/:customerID')
     async getCustomer(@Res() res, @Param('customerID') customerID) {
-        const customer = await this.seekerService.getCustomer(customerID);
-        if (!customer) throw new NotFoundException('Customer does not exist!');
-        return res.status(HttpStatus.OK).json(customer);
+        const seeker = await this.seekerService.getCustomer(customerID);
+        if (!seeker) throw new NotFoundException('Customer does not exist!');
+        return res.status(HttpStatus.OK).json(seeker);
     }
 }
